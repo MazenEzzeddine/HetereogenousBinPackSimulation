4348187dc75a54a6c8469d899498bbdf5c6e9fa0
m.ezzeddine <mazen.ezzeddine@gmail.com>
2022-09-12 13:24:28 +0200

    implemnet fariness

--- main.py
+++ main.py
@@ -29,7 +29,34 @@ class Bin(object):
 
 
 
+class FairBin(object):
+    """ Container for items that keeps a running sum """
+    def __init__(self, capacity):
+        self.items = []
+        self.sum = 0
+        self.capacity = capacity
 
+    def append(self, item):
+        self.items.append(item)
+        self.sum += item
+
+    def remainingCapacity(self):
+        var = self.capacity - self.sum
+        return var
+
+
+    def __str__(self):
+        """ Printable representation """
+        return 'Bin(capacity= %d sum=%d, items=%s)' % ( self.capacity, self.sum, str(self.items))
+
+    def __eq__(self, other):
+        return self.capacity - sum(self.items) == other.capacity- sum(other.items)
+
+    def __lt__(self, other):
+        return self.capacity - sum(self.items) < other.capacity - sum(other.items)
+
+    def __gt__(self, other):
+        return self.capacity - sum(self.items) > other.capacity - sum(other.items)
 
 
 
@@ -60,8 +87,8 @@ def firstFitDec2(weight, n, c):
         if (j == len(bins)):
             bins.append(Bin(c))
             bins[j].append(weight[i])
-    for b in bins:
-        print(b)
+    # for b in bins:
+    #     print(b)
     return bins
 
 
@@ -80,15 +107,29 @@ def firstFitDecHetero(weight, n, c):
     newbins=[]
     for b in bins:
         for c in bins_capacity:
-            if b.sum < c:
+            if b.sum <= c:
                 nb = Bin(c)
                 nb.items.append(b.items)
                 nb.sum = b.sum
                 newbins.append(nb)
                 break
-
+    fairbins = []
     for b in newbins:
+        fairbins.append(FairBin(b.capacity))
         print(b)
+
+    for b in fairbins:
+        print(b)
+
+    for i in range(n):
+        fairbins.sort(reverse=True)
+        fairbins[0].append(weight[i])
+    for b in fairbins:
+        print(b)
+
+
+
+
 
 
 
@@ -160,15 +201,12 @@ def firstFitDec(weight, n, c):
 
     res = 0
 
-    # Create an array to store remaining space in bins
-    # there can be at most n bins
     bin_rem = [0] * n
 
     # Place items one by one
     for i in range(n):
 
-        # Find the first bin that can accommodate
-        # weight[i]
+
         j = 0
         while (j < res):
             if (bin_rem[j] >= weight[i]):
@@ -197,14 +235,28 @@ if __name__ == '__main__':
     c = 10
     n = len(weight)
     firstFitDec2(weight, n, 10)
+    print("====================")
     aList = [70, 40, 30, 20, 30, 20]
+    aList2 = [70, 40, 30, 20, 10, 20]
+
     n= len(aList)
     firstFitDec2(aList, 6, 100)
+    print("====================")
+
     firstFitDecLeastLoaded(aList, 6, 100)
+    print("====================")
+
     firstFitDecHetero(aList, 6, 100)
+    print("====================")
 
     print("-------------------------")
     firstFitDecHetero(aList, 6, 150)
+    print("-------------------------")
+    firstFitDecHetero(aList2, 6, 150)
+
+    # print("===================")
+    # firstFitDecLeastLoaded(aList2, 6, 100)
+
 
 
     #print("Number of bins required in First Fit Decreasing : ", str(firstFitDec(weight, n, c)))
